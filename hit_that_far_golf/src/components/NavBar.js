0356abf78331fd8cb856fb9ffb4b24dfc643a673
austinp <auspendleton@gmail.com>
2024-03-09 19:49:56 -0700

    Started Media Queries

--- hit_that_far_golf/src/components/NavBar.js
+++ hit_that_far_golf/src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <div className="navbar">
       <div className="navbar__container">
-        <div className="logo__conatainer">
+        <div className="logo__container">
           <img className="logo" src={logo} />
         </div>
         <div className="navbar__buttons">
